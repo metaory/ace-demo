1e2a9165a8087aa0a8d52a1266f039616f02da84
metaory <metaory@gmail.com>
2023-07-12 13:42:32 +0800

    added auth guard

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,19 @@
 <script setup>
+import { Authenticator } from '@aws-amplify/ui-vue'
+import '@aws-amplify/ui-vue/styles.css'
 import HelloWorld from './components/HelloWorld.vue'
 import TheWelcome from './components/TheWelcome.vue'
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
+  <authenticator>
+    <template v-slot="{ user, signOut }">
+      <h1>Hello {{ user.username }}!</h1>
+      <button @click="signOut">Sign Out</button>
       <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+      <TheWelcome />
+    </template>
+  </authenticator>
 </template>
 
 <style scoped>
