0c08f4d277a7bcade51f1f570776650d6baac63f
metaory <metaory@gmail.com>
2023-07-14 20:24:33 +0800

    wip

--- amplify/backend/function/acedemo46b9fae0/src/index.js
+++ amplify/backend/function/acedemo46b9fae0/src/index.js
@@ -1,36 +1,33 @@
-const Chance = require("chance");
-const chance = new Chance();
+const Chance = require('chance')
+const chance = new Chance()
 
 /**
  * @type {import('@types/aws-lambda').APIGatewayProxyHandler}
  */
 const headers = {
-	"Access-Control-Allow-Origin": "*",
-	"Access-Control-Allow-Headers": "*",
-};
+  'Access-Control-Allow-Origin': '*',
+  'Access-Control-Allow-Headers': '*'
+}
 
-const rnd = (scale) => Math.floor(Math.random() * scale);
+const rnd = (max, min = 3) => Math.floor(Math.random() * max) + min
 
-const rndArray = (length = 7) => Array.from({ length }).map(() => rnd(100));
+const rndArray = (length = 7) => Array.from({ length }).map(() => rnd(100))
 
 const makeResponse = (data) => ({
-	statusCode: 200,
-	headers,
-	body: JSON.stringify(data),
-});
+  statusCode: 200,
+  headers,
+  body: JSON.stringify(data)
+})
 
 exports.handler = async (event) => {
-	console.log(`EVENT: ${JSON.stringify(event)}`);
-	const { queryStringParameters } = event;
-	const { type } = queryStringParameters ?? { type: "animal" };
+  console.log(`EVENT: ${JSON.stringify(event)}`)
+  const { queryStringParameters } = event
+  const { type } = queryStringParameters ?? { type: 'animal' }
 
-	const lineChart = rndArray();
-	const radar = [rndArray(), rndArray()];
+  const lineChart = rndArray()
+  const radar = [rndArray(), rndArray()]
 
-	const pieChart = Array.from({ length: rnd(6) }).map(() => [
-		chance[type](),
-		rnd(100),
-	]);
+  const pieChart = Array.from({ length: rnd(6) }).map(() => [chance[type](), rnd(100)])
 
-	return makeResponse({ lineChart, radar, pieChart });
-};
+  return makeResponse({ lineChart, radar, pieChart })
+}
--- tmp.js
+++ tmp.js
@@ -1,21 +1,18 @@
-const Chance = require("chance");
-const chance = new Chance();
+const Chance = require('chance')
+const chance = new Chance()
 
 // [ ['Blueberry', 44], ['Strawberry', 23] ]
 
-const data = Array.from({ length: 4 }).map(() => [
-	chance.first(),
-	Math.floor(Math.random() * 100),
-]);
-console.log("data:", data);
+const data = Array.from({ length: 4 }).map(() => [chance.first(), Math.floor(Math.random() * 100)])
+console.log('data:', data)
 
-const rnd = Math.floor(Math.random() * 10);
-
-console.log("rnd:", rnd);
-
-const event = { queryStringParameters: { type: "foo" } };
+const event = { queryStringParameters: { type: 'foo' } }
 // const event = { queryStringParameters: null };
 
-const { queryStringParameters } = event;
-const { type } = queryStringParameters ?? { type: "xx" };
-console.log("type:", type);
+const { queryStringParameters } = event
+const { type } = queryStringParameters ?? { type: 'xx' }
+console.log('type:', type)
+
+const rnd = (max, min = 3) => Math.floor(Math.random() * max) + min
+
+console.log(rnd(10))
