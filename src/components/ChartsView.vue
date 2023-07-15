170ff5f5b577c2504bb6b459cf7c6ef3cbc0cbdb
metaory <metaory@gmail.com>
2023-07-15 14:47:05 +0800

    wip

--- src/components/ChartsView.vue
+++ src/components/ChartsView.vue
@@ -1,28 +1,29 @@
 <script setup>
-import { API } from 'aws-amplify'
-import { ref } from 'vue'
+import { API } from "aws-amplify";
+import { ref } from "vue";
+import LoadingView from "./LoadingView.vue";
 
-const chartData = ref([])
-const picked = ref('animal')
+const chartData = ref([]);
+const picked = ref("animal");
+const loading = ref(false);
 
 function loadData() {
-  const apiName = 'aceapi'
-  const path = `/items?type=${picked.value}`
-  console.log('path:', path)
+  loading.value = true;
+  const apiName = "aceapi";
+  const path = `/items?type=${picked.value}`;
   API.get(apiName, path)
     .then((res) => {
-      console.log('@>', res)
-      chartData.value = res.pieChart
+      console.log("@>", res);
+      chartData.value = res.pieChart;
+      loading.value = false;
     })
-    .catch(console.error)
+    .catch((e) => {
+      console.error(e);
+      loading.value = false;
+    });
 }
 
-function onChange() {
-  console.log('ON CHANGE', picked.value)
-  loadData()
-}
-
-loadData()
+loadData();
 </script>
 
 <template>
@@ -30,28 +31,21 @@ loadData()
     <div>
       <button @click="loadData">reload</button>
       <div>Picked: {{ picked }}</div>
+      <h3>{{ loading }}</h3>
 
-      <input
-        v-on:change="onChange"
-        type="radio"
-        id="animal"
-        value="animal"
-        v-model="picked"
-        :checked="true"
-      />
-      <label for="animal">Animals</label>
+      <form action="">
+        <label class="form-control">
+          <input type="radio" value="animal" v-model="picked" v-on:change="loadData" checked />
+          animal
+        </label>
 
-      <input
-        v-on:change="onChange"
-        type="radio"
-        id="profession"
-        value="profession"
-        v-model="picked"
-        :checked="false"
-      />
-      <label for="city">Profession</label>
+        <label class="form-control">
+          <input type="radio" value="profession" v-model="picked" v-on:change="loadData" />
+          profession
+        </label>
+      </form>
     </div>
-
+    <LoadingView v-if="loading" />
     <pie-chart v-if="chartData" :data="chartData"></pie-chart>
   </main>
 </template>
@@ -59,5 +53,9 @@ loadData()
 <style scoped>
 main {
   display: grid;
+}
+
+input[type="radio"] {
+  accent-color: red !important;
 }
 </style>
--- src/components/HeaderView.vue
+++ src/components/HeaderView.vue
@@ -1,10 +1,31 @@
 <script setup>
-defineProps(["user", "signOut"]);
+// import { mount } from "object-visualizer";
+import { ObjectVisualizer } from "object-visualizer";
+import "object-visualizer/dist/index.min.css";
+
+import { onMounted, onUpdated } from "vue";
+// import { templateRef } from "@vueuse/core";
+const props = defineProps(["user", "signOut"]);
+
+// const target = templateRef("target");
+// const mountLib = () => mount(props.user, target.value, { expandOnCreatedAndUpdated: () => true });
+
+onMounted(() => {
+  // mountLib();
+});
+onUpdated(() => {
+  // mountLib();
+});
 </script>
 
 <template>
   <main>
     <h2>Username: {{ user.username }}</h2>
+    <ObjectVisualizer
+      :data="props.user"
+      rootName="User"
+      :expandOnCreatedAndUpdated="() => false"
+    ></ObjectVisualizer>
     <button @click="signOut">Sign Out</button>
   </main>
 </template>
@@ -12,6 +33,39 @@ defineProps(["user", "signOut"]);
 <style scoped>
 main {
   display: grid;
-  grid-template-columns: 3fr 1fr;
+  grid-template-columns: 2fr 4fr 1fr;
+}
+button {
+  display: inline-block;
+  padding: 10px 20px;
+  height: 3em;
+  font-size: 24px;
+  font-weight: bold;
+  text-align: center;
+  text-decoration: none;
+  color: #fff;
+  background-color: #ff5252;
+  border: 2px solid #000;
+  border-radius: 10px;
+  box-shadow: 5px 5px 0px #000;
+  transition: all 0.3s ease;
+}
+
+button:hover {
+  background-color: #fff;
+  color: #ff5252;
+  border: 2px solid #ff5252;
+  box-shadow: 5px 5px 0px #ff5252;
+}
+
+button:active {
+  background-color: #fcf414;
+  box-shadow: none;
+  transform: translateY(4px);
+}
+
+.object-visualizer {
+  background-color: #fff;
+  font-family: arial;
 }
 </style>
